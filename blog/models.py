d188bbb9e60dff85cc0843783312d99b1f37e1bc
Samir Mammeri <samir.mammeri@nokia.com>
2020-10-08 13:45:44 +0200

    Added PostAdmin in admin interface + adde tinymce plugging for hmtl format support in post's content

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 STATUS = (
     (0,"Draft"),
@@ -12,9 +13,10 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    tag = models.SlugField(max_length=200, unique=True, default="test_debug")
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
-    content = models.TextField()
+    content = HTMLField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
