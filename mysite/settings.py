1a39c7dea863833e63694e196f5853536d0fb745
Samir Mammeri <samir.mammeri@nokia.com>
2020-10-08 14:13:45 +0200

    Added code snippet support in HTMLField

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+## TinyMCE
+TINYMCE_DEFAULT_CONFIG = {
+'plugins': "table,codesample",
+}
