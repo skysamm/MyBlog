700dcbaa6fc722799ebeca7dd2096d3ce9773954
Samir Mammeri <samir.mammeri@nokia.com>
2020-10-08 16:20:22 +0200

    Fixed HTMLField representation and added post_detail template

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = HTMLField()
+    hook = models.TextField(max_length=200, default=" ")
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
