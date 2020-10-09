3b02f7d1acaf0343439b4057e79a172bea2adf20
Samir Mammeri <samir.mammeri@nokia.com>
2020-10-09 10:25:03 +0200

    Added pagination in index view

--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,7 @@ from .models import Post
 class PostList(generic.ListView):
 	queryset = Post.objects.filter(status=1).order_by('-created_on')
 	template_name = 'index.html'
-
-"""class PostDetail(generic.DetailView):
-	model = Post
-	template_name = 'post_detail.html'"""
+	paginate_by = 5
 
 def get_contact(request):
     return render(request, 'contact.html')
