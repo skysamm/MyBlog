7bc5c32d8220a65d12219a7c134a756cf86f3e97
Samir Mammeri <samir.mammeri@nokia.com>
2020-10-09 09:34:43 +0200

    Added contact tab

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path(r'contact/$', views.get_contact, name='contact'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponse
+from django.shortcuts import render
+
 from .models import Post
+import datetime
+
 
 class PostList(generic.ListView):
 	queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -9,3 +14,6 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
 	model = Post
 	template_name = 'post_detail.html'
+
+def get_contact(request):
+    return render(request, 'contact.html')
