19343a9bebab27fd6f5e9492b94e0bfe8a8c449f
Samir Mammeri <samir.mammeri@nokia.com>
2020-10-09 10:51:39 +0200

    Added static file directory and change style pointing in base.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#Location of static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 
 ## TinyMCE
